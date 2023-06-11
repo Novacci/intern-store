a8bd177a240c8dee7cb0615ad69ca083b79ae9a8
Wojciech Nowak <wojtekn00@gmail.com>
2023-06-11 14:46:25 +0200

    Added navbar, fonts and changed favicon

---
+++ app/components/InfoNavBar.tsx
@@ -0,0 +1,11 @@
+import '../styles/InfoNavBar.scss';
+
+export default function InfoNavBar() {
+  return (
+    <div className="nav-container">
+      <span>fast shipping </span>
+      <span>• lifetime warranty </span>
+      <span>• 14-day returns</span>
+    </div>
+  );
+}
