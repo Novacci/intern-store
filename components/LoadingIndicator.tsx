fac2c93f59a845f4a56bf6560a1692a648befac3
Wojciech Nowak <wojtekn00@gmail.com>
2023-07-26 16:09:56 +0200

    Created Loading Indicator component with styling

---
+++ components/LoadingIndicator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '@/app/styles/LoadingIndicator.scss';
+
+export default function LoadingIndicator() {
+  return (
+    <div>
+      <span className="loader"></span>
+    </div>
+  );
+}
