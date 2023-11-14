0ece061f12422bfd336a5cb84c3f8ce7a5e6c98f
Wojciech Nowak <wojtekn00@gmail.com>
2023-11-14 14:36:08 +0100

    Disabling button of applying discount code when used once

--- components/Cart/CartModal.tsx
+++ components/Cart/CartModal.tsx
@@ -180,6 +180,7 @@ export default function CartModal(props: Product) {
           onClick={discoutCodeValidator}
           className="middle none center rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           data-ripple-light="true"
+          disabled={discountAlreadyUsed}
         >
           APPLY DISCOUT CODE
         </button>
